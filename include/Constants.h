9071c47a986b4090a10ca655fc5e94f7efa587d5
Nguyễn Đức Bảo Trung <25521963@gm.uit.edu.vn>
2026-04-20 15:12:04 +0700

    add comment

--- include/Constants.h
+++ include/Constants.h
@@ -1,5 +1,4 @@
 #pragma once
 
-/* QA: add context usage for each constant*/
-constexpr double WIN_RATE = 0.50;
+constexpr double WIN_RATE = 0.5;
 constexpr double COMMISSION = 0.05;
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,22 +1,42 @@
 #include "../include/Player.h"
 
 /* *
- * QA: add comments
+ * @brief Khởi tạo đối tượng Player với số dư ban đầu.
+ * @param initial_balance Số tiền ban đầu trong tài khoản của người chơi.
+ * @return None Không có.
  * */
-
-Player::Player(double initial_balance) : current_bet(BetType::Xiu, 0.0) {
+Player::Player(double initial_balance) : current_bet(BetType::Xiu, 0.0)
+{
     setBalance(initial_balance);
     strategy = nullptr;
 }
 
-void Player::setStrategy(unique_ptr<ABettingStrategy> newStrategy) {
+/* *
+ * @brief Thiết lập chiến thuật đặt cược mới cho người chơi.
+ * @param newStrategy Con trỏ thông minh (unique_ptr) quản lý chiến thuật mới.
+ * @return void Không trả về giá trị.
+ * */
+void Player::setStrategy(unique_ptr<ABettingStrategy> newStrategy)
+{
     strategy = std::move(newStrategy);
 }
 
-void Player::placeBet(BetResult const &prev_result) {
-    if (strategy != nullptr) {
+/* *
+ * @brief Thực hiện đặt cược cho lượt tiếp theo dựa trên kết quả trước đó.
+ * @param prev_result Kết quả của lượt cược vừa diễn ra.
+ * @return void Không trả về giá trị.
+ * */
+void Player::placeBet(BetResult const &prev_result)
+{
+    if (strategy != nullptr)
+    {
         current_bet = strategy->calNextBet(prev_result);
     }
 }
 
+/* *
+ * @brief Lấy thông tin về lượt cược hiện tại của người chơi.
+ * @param Không có tham số.
+ * @return Bet Đối tượng Bet chứa cửa đặt và số tiền cược hiện tại.
+ * */
 Bet Player::getCurrentBet() const { return current_bet; }
